5ce75479636b8c69b7ea8db6d628f9ed53eb646c
Kevin James Lausen <opensourceconstruction@gmail.com>
2022-06-07 08:33:11 -0400

    fixed an accident that I got double options during a commit.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -55,10 +55,6 @@ return packer.startup(function(use)
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
 
-  -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
-
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
