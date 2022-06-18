89203808ac3bdb7c67422a97525daf70721c256e
Kevin James Lausen <opensourceconstruction@gmail.com>
2022-06-18 13:51:08 -0400

    I added a much better indent-line configuration. I am still getting used to the standard commit, being inside my tweaked out Neo-vim. I'm going to try to better document my attempts with better commit messages. I enabled L-til for Leader-Toggle-Indent-Lines, to Toggle indention-lines. I Love a super blingy shell.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -165,3 +165,6 @@ keymap("i", "jk", "<ESC>", opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Indent-Lines-Toggle:
+  keymap("n", "<leader>ilt", ":IndentLinesToggle<CR>", opts)
