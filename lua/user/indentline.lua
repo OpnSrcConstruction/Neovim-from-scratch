a1d7ccb1240ff9b7ae9b13a52b0d30a5fce5479f
Kevin James Lausen <opensourceconstruction@gmail.com>
2022-06-28 19:15:45 -0400

    Minor code cleanup.

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -8,7 +8,6 @@ vim.g.indentLine_char_list = { "|", "¦", "┆", "┊", }
 vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 vim.g.indent_blankline_filetype_exclude = {
   "help",
-  "startify",
   "dashboard",
   "packer",
   "neogitstatus",
@@ -17,7 +16,7 @@ vim.g.indent_blankline_filetype_exclude = {
 }
 vim.g.indentLine_enabled = 1
 -- vim.g.indent_blankline_char = "│"
-vim.g.indent_blankline_char = "▏"
+-- vim.g.indent_blankline_char = "▏"
 -- vim.g.indent_blankline_char = "▎"
 vim.g.indent_blankline_show_trailing_blankline_indent = true
 vim.g.indent_blankline_show_first_indent_level = true
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -57,3 +57,5 @@ local options = {
 -- vim.cmd Passes strings, that will be interpreted as vim-script.
   vim.cmd "set whichwrap+=<,>,[,],h,l" -- This wraps lines with w/ h-l/<-|-> arrows in command/insert-mode.
   vim.cmd [[set iskeyword+=-]] -- Will treat words with a dash as part of a single word.
+
+  vim.g.user_emmet_leader_key = ""
