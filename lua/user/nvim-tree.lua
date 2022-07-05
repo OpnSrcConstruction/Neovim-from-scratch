eca3c8053a626533f91c075e0be6bcd9386563ab
Kevin James Lausen <OpnSrcConstruction@gmail.com>
2022-07-05 02:49:43 -0400

    Fixed the last error of the first page of errors.

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -104,11 +104,11 @@ nvim_tree.setup {
   -- git_hl = 1,
   -- disable_window_picker = 0,
   -- root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 42,
-  },
+  -- show_icons = {
+  --   git = 1,
+  --   folders = 1,
+  --   files = 1,
+  --   folder_arrows = 1,
+  --   tree_width = 42,
+  -- },
 }
