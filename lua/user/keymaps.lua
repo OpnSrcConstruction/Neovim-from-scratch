413f22052152a6749218eb21d4f0e710af141032
Kevin James Lausen <opensourceconstruction@gmail.com>
2022-07-16 10:33:09 -0400

    ♻️ Major Comment cleanup for my keymaps file.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -85,20 +85,20 @@
 -- ## Map nn, to toggle regular line-numberings on,          #
 -- ## to check for which specific line of the file your on. #
 -- ##########################################################
-  keymap("n", "<leader>lno", ":set nonu<CR>", opts) -- Turn line-numbers OFF.
-  keymap("n", "<leader>ln", ":set nu<CR>", opts)    -- Turn line-numbers back on.
+  keymap("n", "<leader>lno", ":set nonu<CR>", opts)  -- Turn line-numbers OFF.
+  keymap("n", "<leader>ln", ":set nu<CR>", opts)     -- Turn line-numbers back on.
 
 -- ###################################################################
 -- ## Map nr to toggle off the relative line numbers in Normal mode. #
 -- ###################################################################
-  keymap("n", "<leader>rlno", ":set nornu<CR>", opts) -- Turn relative line-numbers OFF.
-  keymap("n", "<leader>rln", ":set rnu<CR>", opts)  -- Turn relative line-numbers back on.
+  keymap("n", "<leader>rlno", ":set nornu<CR>", opts)  -- Turn relative line-numbers OFF.
+  keymap("n", "<leader>rln", ":set rnu<CR>", opts)     -- Turn relative line-numbers back on.
 
 -- ###########################################################################
 -- ##I'm mapping comma{,} + lower-case 'l' to disable special char listings. #
 -- ###########################################################################
-  keymap("n", "<leader>nls", ":set nolist<CR>", opts) -- Turn special hidden chars OFF.
-  keymap("n", "<leader>ls", ":set list<CR>", opts)    -- Turn List(ls) mode back on.
+  keymap("n", "<leader>nls", ":set nolist<CR>", opts)  -- Turn special hidden chars OFF.
+  keymap("n", "<leader>ls", ":set list<CR>", opts)     -- Turn List(ls) mode back on.
 
 -- ###############################################################################
 -- ## cm == copy-mode. disables special characters($) and relative-line-numbers. #
@@ -127,24 +127,21 @@
 -- ## qa == :qa! aka quit the file without saving                          #
 -- ## w == :w <ENTER> aka save(write) file.                                #
 -- #########################################################################
-  keymap("n", "<leader>qq", ":q<CR>", opts)
+  -- keymap("n", "<leader>qq", ":q<CR>", opts)
   keymap("n", "<leader>wq", ":wq<CR>", opts)
-  keymap("n", "<leader>qall", ":qa!<CR>", opts)
-  keymap("n", "<leader>save", ":w<CR>", opts)
+  keymap("n", "<leader>qal", ":qa!<CR>", opts)
+  -- keymap("n", "<leader>save", ":w<CR>", opts)
 
 -- ##################
 -- ## Mark-keymaps: #
 -- ##################
-  keymap("n", "<leader>dmrks", ":delmarks!<CR>", opts)     -- Delete ALL marks:
+  keymap("n", "<leader>dmrks", ":delmarks!<CR>", opts)  -- Delete ALL marks:
 
 -- #################
 -- # TAB keymaps:  #
 -- #################
-  keymap("n", "<leader>tn", ":tabnew ", opts)          -- New-tab
-  keymap("n", "<leader>x", ":Bdelete<CR>", opts)      -- Close-tab
-  -- keymap("n", "<leader>bt", "gT", opts)             -- Backward-tab(s)
-  -- keymap("n", "<leader>ft", "gt", opts)             -- Forward-tab(s)
-  -- redundant keymap("n", "<leader>db", ":bd<CR>", opts)        -- Delete-Buffer
+  keymap("n", "<leader>tn", ":tabnew ", opts)     -- New-tab
+  keymap("n", "<leader>x", ":Bdelete<CR>", opts)  -- Close-tab
 
 -- ################################################################################
 -- # Split-keymaps:                                                               #
@@ -155,14 +152,9 @@
 -- # Although clink 4 cmd does have history/jab-control, but sadly no shell-view  #
 -- # multi-plexing. :(                                                            #
 -- ################################################################################
+  keymap("n", "<leader>spt", ":split<CR>", opts)    -- Splits current view horizontally.
+  keymap("n", "<leader>vspt", ":vsplit<CR>", opts)  -- Splits current view Vertically.
 
-  keymap("n", "<leader>spt", ":split<CR>", opts)     -- Splits current view horizontally.
-  keymap("n", "<leader>vspt", ":vsplit<CR>", opts)    -- Splits current view Vertically.
-
-
--- Better terminal navigation... ???
--- This is the ONLY block I am not sure if I want to include in my config,
--- b/c I don't understand what it does; so I'll disable it for now...
 -- Terminal --
 -- Better terminal navigation:
   keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
@@ -179,20 +171,15 @@
 -- #########################################################
 -- ##  Toggle Alpha for ascii comic relief & bookmarks.   ##
 -- #########################################################
-  keymap("n", "<leader>aa", ":Alpha<CR>", opts)     -- Delete ALL marks:
+  keymap("n", "<leader>aa", ":Alpha<CR>", opts)  -- Delete ALL marks:
 
 -- Toggle-term TUI-app maps:
-  keymap("n", "<leader>lgIt", ":lua _LAZYGIT_TOGGLE()<CR>", opts)    -- Maps L->lgt to lazygit.
-
-  keymap("n", "<leader>ncdu", ":lua _NCDU_TOGGLE()<CR>", opts)      -- Maps L->ncdu to ncdu fs analyzer.
-
-  keymap("n", "<leader>htop", ":lua _HTOP_TOGGLE()<CR>", opts)      -- maps L->htop to my htop system monitor.
-
-  keymap("n", "<leader>node", ":lua _NODE_TOGGLE()<CR>", opts)      -- Maps L->node to the NodeJS pkg. mgr.
-
-  keymap("n", "<leader>py3", ":lua _PYTHON_TOGGLE()<CR>", opts)     -- Maps L->py3 to the Basic IDLE python-3 shell.
-
-  keymap("n", "<leader>nmon", ":lua _NMON_TOGGLE()<CR>", opts)      -- Great sytem-monitoring tool.
+  -- keymap("n", "<leader>lgIt", ":lua _LAZYGIT_TOGGLE()<CR>", opts)   -- Maps L->lgt to lazygit.
+  -- keymap("n", "<leader>ncdu", ":lua _NCDU_TOGGLE()<CR>", opts)      -- Maps L->ncdu to ncdu fs analyzer.
+  -- keymap("n", "<leader>htop", ":lua _HTOP_TOGGLE()<CR>", opts)      -- maps L->htop to my htop system monitor.
+  -- keymap("n", "<leader>node", ":lua _NODE_TOGGLE()<CR>", opts)      -- Maps L->node to the NodeJS pkg. mgr.
+  -- keymap("n", "<leader>py3", ":lua _PYTHON_TOGGLE()<CR>", opts)     -- Maps L->py3 to the Basic IDLE python-3 shell.
+  -- keymap("n", "<leader>nmon", ":lua _NMON_TOGGLE()<CR>", opts)      -- Great sytem-monitoring tool.
   -- Some basic keys after your in nmon:
   -- c == CPU monitor
   -- d == Disk I/O
@@ -200,44 +187,43 @@
   -- n == NT
   -- q == quit
   -- ? == help-menu
-
-  -- keymap("n", "<leader>ipy3", ":lua _IPYTHON_TOGGLE()<CR>", opts)      -- Great sytem-monitoring tool.
-  -- keymap("n", "<leader>tre", ":UndotreeToggle<CR>", opts)      -- Amazing Vim Undoo-tree visualization tool.
-  -- keymap("n", "<leader>tbar", ":TagbarToggle<CR>", opts)      -- Amazing Vim Undoo-tree visualization tool.
-  -- keymap("n", "<leader>vifm", ":lua _VIFM_TOGGLE()<CR>", opts)      -- Amazing Vim Undoo-tree visualization tool.
+  -- keymap("n", "<leader>ipy3", ":lua _IPYTHON_TOGGLE()<CR>", opts)  -- Great sytem-monitoring tool.
+  -- keymap("n", "<leader>tre", ":UndotreeToggle<CR>", opts)          -- Amazing Vim Undoo-tree visualization tool.
+  -- keymap("n", "<leader>tbar", ":TagbarToggle<CR>", opts)           -- Amazing Vim Undoo-tree visualization tool.
+  -- keymap("n", "<leader>vifm", ":lua _VIFM_TOGGLE()<CR>", opts)     -- Amazing Vim Undoo-tree visualization tool.
 
 -- Git-Signs(lua-gutter):
 -- Hunk-navigation:
   keymap("n", "<leader>gsnh", ":Gitsigns next_hunk<CR>", opts)  -- Move to next code-hunk.
   keymap("n", "<leader>gsph", ":Gitsigns prev_hunk<CR>", opts)  -- Move to a previous code-hunk.
 
-  keymap("n", "<leader>gssh", ":Gitsigns select_hunk<CR>", opts)   -- Select hunk in Visual-mode.
-  keymap("n", "<leader>gspvh", ":Gitsigns preview_hunk<CR>", opts)   -- Preview hunk-diff preview.
+  keymap("n", "<leader>gssh", ":Gitsigns select_hunk<CR>", opts)      -- Select hunk in Visual-mode.
+  keymap("n", "<leader>gspvh", ":Gitsigns preview_hunk<CR>", opts)    -- Preview hunk-diff preview.
   keymap("n", "<leader>gstd", ":Gitsigns toggle_deleted<CR>", opts)   -- Toggle recently deleted hunks.
   -- Great as a what did I just do toggle through git and NOT Undo/Redo.
-  keymap("n", "<leader>gsbl", ":Gitsigns blame_line<CR>", opts)  -- Runs blame_line.
-  keymap("n", "<leader>gssb", ":Gitsigns stage_buffer<CR>", opts)  -- Stage All hunks in current buffer.
+  keymap("n", "<leader>gsbl", ":Gitsigns blame_line<CR>", opts)           -- Runs blame_line.
+  keymap("n", "<leader>gssb", ":Gitsigns stage_buffer<CR>", opts)         -- Stage All hunks in current buffer.
   keymap("n", "<leader>gsrbi", ":Gitsigns reset_buffer_index<CR>", opts)  -- Unstage ALL hunks for current buffer/tab in the index.
   -- This runs a full file reset.
-  keymap("n", "<leader>gstgh", ":Gitsigns stage_hunk<CR>", opts)  -- Stage a single hunk under cursor.
-  keymap("n", "<leader>gsush", ":Gitsigns undo_stage_hunk<CR", opts)  -- Reset a staged hunk.
+  keymap("n", "<leader>gstgh", ":Gitsigns stage_hunk<CR>", opts)        -- Stage a single hunk under cursor.
+  keymap("n", "<leader>gsush", ":Gitsigns undo_stage_hunk<CR", opts)    -- Reset a staged hunk.
   keymap("n", "<leader>gstwd", ":Gitsigns toggle_word_diff<CR>", opts)  -- Toggle word diff highlighting-mode.
-  keymap("n", "<leader>gsqfl", ":Gitsigns setqflist<CR>", opts)  -- Populate the quickfix list with change-hunks.
-  keymap("n", "<leader>gslls", ":Gitsigns setloclist<CR>", opts)  -- Populate the quickfix list with change-hunks.
+  keymap("n", "<leader>gsqfl", ":Gitsigns setqflist<CR>", opts)         -- Populate the quickfix list with change-hunks.
+  keymap("n", "<leader>gslls", ":Gitsigns setloclist<CR>", opts)        -- Populate the quickfix list with change-hunks.
 -- Telescope fuzzy-searching HKs:
-  keymap("n", "<leader>tscp", "<cmd>Telescope<CR>", opts)  -- For searching through available telescope commandaaaaaas.
-  keymap("n", "<leader>tsfd", "<cmd>Telescope find_files<cr>", opts) -- Search for files, but respecting .gitignore.
-  keymap("n", "<leader>tslg", "<cmd>Telescope live_grep<cr>", opts) -- For rip-grepping(rg) through open project buffers.
-  -- keymap("n", "<leader>tskm", "<cmd>Telescope keymaps<CR>", opts) -- For searching Key-maps.
-  -- keymap("n", "<leader>tscmds", "<cmd>Telescope commands<CR>", opts) -- Search for available ex-mode commands.
-  keymap("n", "<leader>tscmdh", "<cmd>Telescope command_history<CR>", opts) -- Search for available ex-mode commands.
-  keymap("n", "<leader>tswd", "<cmd>Telescope diagnostics<CR>", opts)  -- Search for work-place diagnostics(errors).
-  keymap("n", "<leader>tsac", "<cmd>Telescope autocommands<CR>", opts)  -- Search through loaded auto-commands I think.
-  -- keymap("n", "<leader>tsmp", "<cmd>Telescope man_pages<CR>", opts)  -- Search through available man pages.
-  keymap("n", "<leader>tspj", "<cmd>Telescope projects<CR>", opts)  -- Search through ongoing projects, by f-system location.
-  keymap("n", "<leader>tvim", "<cmd>Telescope vim_options<CR>", opts)  -- Search through available vim options.
-  -- keymap("n", "<leader>tsof", "<cmd>Telescope oldfiles<CR>", opts)  -- Search through Files of a project old w aest to newest.📁
-  keymap("n", "<leader>tsft", "<cmd>Telescope filetypes<CR>", opts)  -- Search through File-types.📁
+  keymap("n", "<leader>tscp", "<cmd>Telescope<CR>", opts)                    -- For searching through available telescope commands.
+  keymap("n", "<leader>tsfd", "<cmd>Telescope find_files<cr>", opts)         -- Search for files, but respecting .gitignore.
+  keymap("n", "<leader>tslg", "<cmd>Telescope live_grep<cr>", opts)          -- For rip-grepping(rg) through open project buffers.
+  -- keymap("n", "<leader>tskm", "<cmd>Telescope keymaps<CR>", opts)         -- For searching Key-maps.
+  -- keymap("n", "<leader>tscmds", "<cmd>Telescope commands<CR>", opts)      -- Search for available ex-mode commands.
+  keymap("n", "<leader>tscmdh", "<cmd>Telescope command_history<CR>", opts)  -- Search for available ex-mode commands.
+  keymap("n", "<leader>tswd", "<cmd>Telescope diagnostics<CR>", opts)        -- Search for work-place diagnostics(errors).
+  keymap("n", "<leader>tsac", "<cmd>Telescope autocommands<CR>", opts)       -- Search through loaded auto-commands I think.
+  -- keymap("n", "<leader>tsmp", "<cmd>Telescope man_pages<CR>", opts)       -- Search through available man pages.
+  keymap("n", "<leader>tspj", "<cmd>Telescope projects<CR>", opts)           -- Search through ongoing projects, by f-system location.
+  keymap("n", "<leader>tvim", "<cmd>Telescope vim_options<CR>", opts)        -- Search through available vim options.
+  -- keymap("n", "<leader>tsof", "<cmd>Telescope oldfiles<CR>", opts)        -- Search through Files of a project old w aest to newest.📁
+  keymap("n", "<leader>tsft", "<cmd>Telescope filetypes<CR>", opts)          -- Search through File-types.📁
   -- f in Normal-mode or <C-f> in insert mode to search through/launch project files of selected project.
   -- b in Normal-mode or <C-b> in insert mode to browse project files.
   -- d in Normal-mode or <C-d> in insert mode to DELETE_PROJECT.
@@ -245,25 +231,25 @@
   -- r in Normal-mode or <C-r> in insert mode to search for MRU(Most_Recently_Used) files.
   -- w in Normal-mode or <C-w> in insert mode to Change the CWD(Current_Working_Directory) of the selected project.
     -- Git...
-  keymap("n", "<leader>tsgf", "<cmd>Telescope git_files<CR", opts)  -- Searches through Git tracked files in this project.
-  keymap("n", "<leader>tscmt", "<cmd>Telescope git_commits<CR>", opts)  -- Searches through Git commits in this directory.
+  keymap("n", "<leader>tsgf", "<cmd>Telescope git_files<CR", opts)        -- Searches through Git tracked files in this project.
+  keymap("n", "<leader>tscmt", "<cmd>Telescope git_commits<CR>", opts)    -- Searches through Git commits in this directory.
   keymap("n", "<leader>tsbcmt", "<cmd>Telescope git_bcommits<CR>", opts)  -- Searches through Git commits in this current buffer.
-  keymap("n", "<leader>tsgs", "<cmd>Telescope git_stash<CR>", opts)  -- Lists stash items in current repository.
-  keymap("n", "<leader>tsbr", "<cmd>Telescope git_branches<CR>", opts)  -- Lists branches in current repository.
-  keymap("n", "<leader>tsgs", "<cmd>Telescope git_status<CR>", opts)  -- Lists branches in current repository.
-  keymap("n", "<leader>tgmoji", "<cmd>lua require('telescope').extensions.gitmoji.gitmoji()<CR>", opts)       -- Adds an E-moji to the commit message You choose.
+  keymap("n", "<leader>tsgs", "<cmd>Telescope git_stash<CR>", opts)       -- Lists stash items in current repository.
+  keymap("n", "<leader>tsbr", "<cmd>Telescope git_branches<CR>", opts)    -- Lists branches in current repository.
+  keymap("n", "<leader>tsgs", "<cmd>Telescope git_status<CR>", opts)      -- Lists branches in current repository.
+  keymap("n", "<leader>tgmoji", "<cmd>lua require('telescope').extensions.gitmoji.gitmoji()<CR>", opts)  -- Adds an E-moji to the commit.
   -- Git-hub-cli...
-  keymap("n", "<leader>ghgst", "<cmd>Telescope gh gist", opts)          -- https://cli.github.com/manual/gh_gist Manage Gists.
-  keymap("n", "<leader>ghpr", "<cmd>Telescope gh pull_request", opts)       -- Manage Pull-requests...
-  keymap("n", "<leader>ghprf", "<cmd>Telescope gh pull_request_files", opts)    --Search through the PR-file-set.
-  keymap("n", "<leader>ghs", "<cmd>Telescope gh secret", opts)              -- Manage GH secrets.
-  keymap("n", "<leader>ghi", "<cmd>Telescope gh issues", opts)          -- Manage GH issues.
-  keymap("n", "<leader>ghwfr", "<cmd>Telescope gh run", opts)           -- View details about currently running Workflow-runs.
+  keymap("n", "<leader>ghgst", "<cmd>Telescope gh gist", opts)                -- https://cli.github.com/manual/gh_gist Manage Gists.
+  keymap("n", "<leader>ghpr", "<cmd>Telescope gh pull_request", opts)         -- Manage Pull-requests...
+  keymap("n", "<leader>ghprf", "<cmd>Telescope gh pull_request_files", opts)  --Search through the PR-file-set.
+  keymap("n", "<leader>ghs", "<cmd>Telescope gh secret", opts)                -- Manage GH secrets.
+  keymap("n", "<leader>ghi", "<cmd>Telescope gh issues", opts)                -- Manage GH issues.
+  keymap("n", "<leader>ghwfr", "<cmd>Telescope gh run", opts)                 -- View details about currently running Workflow-runs.
   -- Fuzzy-menus:
-  -- keymap("n", "<leader>tsbf", "<cmd>Telescope buffers<CR>", opts)  -- Search through open buffers/tabs.
+  -- keymap("n", "<leader>tsbf", "<cmd>Telescope buffers<CR>", opts)     -- Search through open buffers/tabs.
   -- keymap("n", "<leader>tsreg", "<cmd>Telescope registers<CR>", opts)  -- Search through open buffer registers in the tree.
-  -- keymap("n", "<leader>tsmrk", "<cmd>Telescope marks<CR>", opts)  -- Search the jump-list, of where your cursor recently moved.
-  -- keymap("n", "<leader>tsjl", "<cmd>Telescope jumplist<CR>", opts)  -- Search the jump-list, of where your cursor recently moved.
-  -- keymap("n", "<leader>tcjihg", "<cmd>Telescope changes<CR>", opts)  -- Search the list of changes, of what you currently changed.
-  keymap("n", "<leader>tscht", "<cmd>Cheatsheet<CR>", opts)            -- Cheat-sheet Telescope plugin.
-  keymap("n", "<leader>temoji", "<cmd>Telescope emoji<CR>", opts)       -- A great emoji-Picker. 😌
+  -- keymap("n", "<leader>tsmrk", "<cmd>Telescope marks<CR>", opts)      -- Search the jump-list, of where your cursor recently moved.
+  -- keymap("n", "<leader>tsjl", "<cmd>Telescope jumplist<CR>", opts)    -- Search the jump-list, of where your cursor recently moved.
+  -- keymap("n", "<leader>tcjihg", "<cmd>Telescope changes<CR>", opts)   -- Search the list of changes, of what you currently changed.
+  keymap("n", "<leader>tscht", "<cmd>Cheatsheet<CR>", opts)              -- Cheat-sheet Telescope plugin.
+  keymap("n", "<leader>temoji", "<cmd>Telescope emoji<CR>", opts)        -- A great emoji-Picker. 😌
