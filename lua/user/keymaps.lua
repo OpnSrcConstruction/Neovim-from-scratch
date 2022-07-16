3527ee2acfbf4f7005bd286a9dea3cef8a5a06dd
Kevin James Lausen <opensourceconstruction@gmail.com>
2022-07-16 06:38:26 -0400

    🔥 Commented-out L->tcjihg.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -288,7 +288,7 @@
   -- keymap("n", "<leader>tsbf", "<cmd>Telescope buffers<CR>", opts)  -- Search through open buffers/tabs.
   -- keymap("n", "<leader>tsreg", "<cmd>Telescope registers<CR>", opts)  -- Search through open buffer registers in the tree.
   -- keymap("n", "<leader>tsmrk", "<cmd>Telescope marks<CR>", opts)  -- Search the jump-list, of where your cursor recently moved.
-  keymap("n", "<leader>tsjl", "<cmd>Telescope jumplist<CR>", opts)  -- Search the jump-list, of where your cursor recently moved.
-  keymap("n", "<leader>tchg", "<cmd>Telescope changes<CR>", opts)  -- Search the jump-list, of where your cursor recently moved.
+  -- keymap("n", "<leader>tsjl", "<cmd>Telescope jumplist<CR>", opts)  -- Search the jump-list, of where your cursor recently moved.
+  -- keymap("n", "<leader>tcjihg", "<cmd>Telescope changes<CR>", opts)  -- Search the list of changes, of what you currently changed.
   keymap("n", "<leader>tscht", "<cmd>Cheatsheet<CR>", opts)            -- Cheat-sheet Telescope plugin.
   keymap("n", "<leader>temoji", "<cmd>Telescope emoji<CR>", opts)       -- A great emoji-Picker. 😌
