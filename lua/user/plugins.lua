b852dcab3d517562d533c0c6b4e1756053b8f482
OpnSrcConstruction <Opensourceconstruction.com>
2023-07-11 07:56:37 -0400

    ⚡️ Removed an auto-session exception directory I do not use.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -129,7 +129,7 @@ return packer.startup(function(use)
      'rmagatti/auto-session',
     config = function()
     require("auto-session").setup { log_level = "error",
-      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+      auto_session_suppress_dirs = { "~/", "~/Downloads", "/"},
     }
   end
 }
